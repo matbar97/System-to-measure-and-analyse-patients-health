aaedec63452750f0e185752456934286f1622f89
Mateusz Barylka <mateusz.barylka97@gmail.com>
2021-01-03 16:13:48 +0100

    Okno Dodaj Pacjenta + Przekierowanie do okna z wynikami

--- app/src/main/java/com/android/telm/PatientCreationActivity.java
+++ app/src/main/java/com/android/telm/PatientCreationActivity.java
@@ -2,13 +2,37 @@ package com.android.telm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PatientCreationActivity extends AppCompatActivity {
+
+    private Button addPatientButton, goToPreviousActivityButton;
+    private EditText nameEditText, birthEditText, peselEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_creation);
+        addPatientButton = findViewById(R.id.addPatientButton);
+        goToPreviousActivityButton = findViewById(R.id.goToPreviousActivityButton);
+        nameEditText = findViewById(R.id.nameEditText);
+        birthEditText = findViewById(R.id.birthEditText);
+        peselEditText = findViewById(R.id.peselEditText);
+        addPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPatientsData();
+            }
+        });
+
+    }
+
+    private void goToPatientsData() {
+        Intent intent = new Intent(this, PatientDataFromDocPointOfViewActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/android/telm/PatientDataFromDocPointOfViewActivity.java
@@ -0,0 +1,14 @@
+package com.android.telm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientDataFromDocPointOfViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_data_from_doc_point_of_view);
+    }
+}
