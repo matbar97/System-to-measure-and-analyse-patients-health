374c75be3474af49fb2e4353ab01bec3aeb72dce
Mateusz Barylka <mateusz.barylka97@gmail.com>
2021-01-03 21:19:36 +0100

    Okno z dodawaniem badaniem i przekierowaniem na Study Review -> zrobione Activity ze Study Review

--- app/src/main/java/com/android/telm/AddStudyActivity.java
+++ app/src/main/java/com/android/telm/AddStudyActivity.java
@@ -2,13 +2,38 @@ package com.android.telm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddStudyActivity extends AppCompatActivity {
+
+    private Button backAddStudyButton, addAnnotationAddStudyButton, applyStudyAddStudyButton;
+    private EditText studyDateAddStudyEditText, doctorNameAddStudyEditText, observationsMultiLineTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_study);
+        backAddStudyButton = findViewById(R.id.backAddStudyButton);
+        addAnnotationAddStudyButton = findViewById(R.id.addAnnotationAddStudyButton);
+        applyStudyAddStudyButton = findViewById(R.id.applyStudyAddStudyButton);
+        studyDateAddStudyEditText = findViewById(R.id.studyDateAddStudyEditText);
+        doctorNameAddStudyEditText = findViewById(R.id.doctorNameAddStudyEditText);
+        observationsMultiLineTextView = findViewById(R.id.observationsAddStudyEditText);
+
+        applyStudyAddStudyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToStudyReview();
+            }
+        });
+    }
+
+    private void goToStudyReview() {
+        Intent intent = new Intent(this, StudyReviewActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/android/telm/StudyReviewActivity.java
@@ -0,0 +1,14 @@
+package com.android.telm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_review);
+    }
+}
