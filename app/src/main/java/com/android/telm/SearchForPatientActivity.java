0d6c6669706d93a7771b0301c6f5b061f3f7d6a6
Mateusz Barylka <mateusz.barylka97@gmail.com>
2021-01-04 17:09:01 +0100

    Dodane Search For Patient Base

--- app/src/main/java/com/android/telm/SearchForPatientActivity.java
+++ app/src/main/java/com/android/telm/SearchForPatientActivity.java
@@ -1,14 +1,20 @@
 package com.android.telm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchForPatientActivity extends AppCompatActivity {
+
+    private Button arrowBackSearchForPatientButton;
+    private EditText searchForPatientEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_for_patient);
+        arrowBackSearchForPatientButton = findViewById(R.id.arrowBackSearchForPatientButton);
+        searchForPatientEditText = findViewById(R.id.searchForPatientEditText);
     }
 }
