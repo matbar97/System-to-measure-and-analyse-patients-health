77bb66f861f69916323b826c4326b6d304a29c29
Mateusz Barylka <mateusz.barylka97@gmail.com>
2021-01-05 23:04:38 +0100

    Dodane Klasy: Patient, Connector, Sender, obsługa połączeń HTTP (autoryzacja po str backend poprawic)

---
+++ app/src/main/java/com/android/telm/Patient.java
@@ -0,0 +1,109 @@
+package com.android.telm;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+public class Patient {
+    public Patient() {
+
+    }
+    private int id;
+    private String name;
+    private String surname;
+    private String pesel;
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", pesel='" + pesel + '\'' +
+                '}';
+    }
+
+    public Patient(String name, String surname, String pesel) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+    }
+
+    public Patient(Patient patient) {
+        this.name = patient.getName();
+        this.pesel = patient.getPesel();
+        this.surname = patient.getSurname();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String packData() {
+        JSONObject jo=new JSONObject();
+        StringBuffer packedData=new StringBuffer();
+
+        try {
+            jo.put("name", name);
+            jo.put("surname", surname);
+            jo.put("pesel", pesel);
+            Boolean firstValue=true;
+
+            Iterator it=jo.keys();
+
+            do {
+                String key = it.next().toString();
+                String value = jo.get(key).toString();
+
+                if(firstValue) {
+                    firstValue=false;
+                } else {
+                    packedData.append("&");
+                }
+
+                packedData.append(URLEncoder.encode(key,"UTF-8"));
+                packedData.append("=");
+                packedData.append(URLEncoder.encode(value,"UTF-8"));
+
+            } while (it.hasNext());
+            {
+                return packedData.toString();
+            }
+
+        } catch (JSONException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/android/telm/PatientCreationActivity.java
+++ app/src/main/java/com/android/telm/PatientCreationActivity.java
@@ -3,7 +3,9 @@ package com.android.telm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +13,8 @@ import android.widget.EditText;
 public class PatientCreationActivity extends AppCompatActivity {
 
     private Button addPatientButton, goToPreviousActivityButton;
-    private EditText nameEditText, birthEditText, peselEditText;
+    private EditText nameEditText, surnameEditText, peselEditText;
+    private String urlAddr = "http://localhost:8080/api/patients";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,16 @@ public class PatientCreationActivity extends AppCompatActivity {
         addPatientButton = findViewById(R.id.addPatientButton);
         goToPreviousActivityButton = findViewById(R.id.goToPreviousActivityButton);
         nameEditText = findViewById(R.id.nameEditText);
-        birthEditText = findViewById(R.id.birthEditText);
+        surnameEditText = findViewById(R.id.surnameEditText);
         peselEditText = findViewById(R.id.peselEditText);
         addPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Sender s = new Sender(PatientCreationActivity.this, urlAddr, nameEditText, peselEditText, surnameEditText);
+                s.execute();
                 goToPatientsData();
             }
         });
-
     }
 
     private void goToPatientsData() {
@@ -36,3 +40,93 @@ public class PatientCreationActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
+//class SendJsonDataToServer extends AsyncTask<String, String, String> {
+//
+//    @Override
+//    protected String doInBackground(Patient... params) {
+//        String JsonResponse = null;
+//        Patient JsonDATA = params[0];
+//        HttpURLConnection urlConnection = null;
+//        BufferedReader reader = null;
+//        try {
+//            URL url = new URL("x");
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//            urlConnection.setDoOutput(true);
+//            // is output buffer writter
+//            urlConnection.setRequestMethod("POST");
+//            urlConnection.setRequestProperty("Content-Type", "application/json");
+//            urlConnection.setRequestProperty("Accept", "application/json");
+////set headers and method
+//            Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+//            writer.write(JsonDATA);
+//// json data
+//            writer.close();
+//            InputStream inputStream = urlConnection.getInputStream();
+////input stream
+//            StringBuffer buffer = new StringBuffer();
+//            if (inputStream == null) {
+//                // Nothing to do.
+//                return null;
+//            }
+//            reader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//            String inputLine;
+//            while ((inputLine = reader.readLine()) != null)
+//                buffer.append(inputLine + "\n");
+//            if (buffer.length() == 0) {
+//                // Stream was empty. No point in parsing.
+//                return null;
+//            }
+//            JsonResponse = buffer.toString();
+////response data
+//            Log.i("Good", JsonResponse);
+//            //send to post execute
+//            return JsonResponse;
+////                return null;
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (urlConnection != null) {
+//                urlConnection.disconnect();
+//            }
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (final IOException e) {
+//                    Log.e("Good", "Error closing stream", e);
+//                }
+//            }
+//        }
+//        return null;
+//    }
+//
+//
+//    @Override
+//    protected void onPostExecute(String s) {
+//
+//    }
+//}
+//
+//    public void senddatatoserver() {
+//        //function in the activity that corresponds to the layout button
+//        name = nameEditText.getText().toString();
+//        surname = surnameEditText.getText().toString();
+//        pesel = peselEditText.getText().toString();
+//        JSONObject post_dict = new JSONObject();
+//
+//        try {
+//            post_dict.put("name", name);
+//            post_dict.put("surname", surname);
+//            post_dict.put("pesel", pesel);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        if (post_dict.length() > 0) {
+////            SendJsonDataToServer sjdts = new SendJsonDataToServer();
+//            new SendJsonDataToServer().execute(String.valueOf(post_dict));
+////            call to async class
+//        }
